53b9c34905ae6df5d39eced31e51ca8ed351a0a8
Shrouk <shorokshabbaan@gmail.com>
2026-09-25 02:03:38 +0300

    feat: Register identity , add lockout , password settings

--- RideMatch.Infrastructure/ServiceExtension/ServiceExtension.cs
+++ RideMatch.Infrastructure/ServiceExtension/ServiceExtension.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RideMatch.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,10 +14,32 @@ namespace RideMatch.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            //SQL connection
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
+
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                //Password Settings
+                options.Password.RequiredLength = 4;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = false;
+
+                //Lockout settings
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+
+            })
+         .AddEntityFrameworkStores<ApplicationDbContext>()
+         .AddDefaultTokenProviders(); ;
+
             return services;
         }
     }
